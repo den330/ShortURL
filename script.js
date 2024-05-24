88858154f5d8fd64009d72764c5a6fab768ec213
den330 <wo131485508@qq.com>
2024-05-24 14:13:54 -0400

    update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
 const input = document.querySelector("input");
 const resultLabel = document.querySelector("p");
 const button = document.querySelector("button");
 const url = "https://api-ssl.bitly.com/v4/shorten";
-const token = process.env.BITLY_TOKEN;
+const token = "insert your own token here";
 const params = {
   domain: "bit.ly",
 };
