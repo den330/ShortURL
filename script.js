fc232e14fdd4790fdf195e73e7f1c1c0de76c471
den330 <wo131485508@qq.com>
2024-05-24 14:17:05 -0400

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.querySelector("input");
 const resultLabel = document.querySelector("p");
 const button = document.querySelector("button");
 const url = "https://api-ssl.bitly.com/v4/shorten";
-const token = "insert your own token here";
+const token = "insert your token here";
 const params = {
   domain: "bit.ly",
 };
@@ -10,15 +10,13 @@ const params = {
 button.addEventListener("click", async () => {
   const completedParams = { ...params, long_url: input.value };
   const authorization = `Bearer ${token}`;
-  const response = await fetch(url, {
+  const responseJSON = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: authorization,
     },
     body: JSON.stringify(completedParams),
-  });
-  const json = await response.json();
-  const value = await json.link;
-  resultLabel.textContent = value;
+  }).then((response) => response.json());
+  resultLabel.textContent = responseJSON.link;
 });
